9ec38013435b3048df1365ae2f930099c6e530e9
Nerwosolek <github@gieorgijewski.pl>
2021-02-19 18:06:09 +0100

    Pistol kills the enemy. dmg based on bullet speed and modifier.

--- Assets/Scripts/Weapons/PistolBullet.cs
+++ Assets/Scripts/Weapons/PistolBullet.cs
@@ -6,6 +6,8 @@ public class PistolBullet : MonoBehaviour
 {
     public float Speed { get; internal set; }
     public float Range { get; internal set; }
+    [SerializeField]
+    private float _dmgModifier;
     private float _distanceTravelled;
 
     void FixedUpdate()
@@ -13,5 +15,13 @@ public class PistolBullet : MonoBehaviour
         transform.position += transform.forward * Speed * Time.fixedDeltaTime;
         _distanceTravelled += Speed * Time.fixedDeltaTime;
         if (_distanceTravelled > Range) Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<HPManager>().TakeDamage(Speed * _dmgModifier);
+        }
     }
 }
